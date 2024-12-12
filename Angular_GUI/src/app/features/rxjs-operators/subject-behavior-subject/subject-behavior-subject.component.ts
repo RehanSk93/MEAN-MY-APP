306e75d190ecbb40e3f12b817c57f715ba7d5e11
Mughla Azam <45681080+RehanSk93@users.noreply.github.com>
2024-12-12 12:20:27 +0530

    UI: Rxjs component clean-up

--- Angular_GUI/src/app/features/rxjs-operators/subject-behavior-subject/subject-behavior-subject.component.ts
+++ Angular_GUI/src/app/features/rxjs-operators/subject-behavior-subject/subject-behavior-subject.component.ts
@@ -4,29 +4,8 @@ import { Subject } from 'rxjs';
 @Component({
   selector: 'app-subject-behavior-subject',
   templateUrl: './subject-behavior-subject.component.html',
-  styleUrls: ['./subject-behavior-subject.component.scss']
+  styleUrls: ['./subject-behavior-subject.component.scss'],
 })
 export class SubjectBehaviorSubjectComponent {
-
-    // Create a new Subject
-    subject$ = new Subject<number>();
-
-    constructor(){}
-  
-    callSubject() {
-      // Subscribe to the Subject
-      this.subject$.subscribe({
-        next: (value) => console.log(`Observer 1: ${value}`),
-      });
-  
-      // Another subscription to the Subject
-      this.subject$.subscribe({
-        next: (value) => console.log(`Observer 2: ${value}`),
-      });
-  
-      this.subject$.next(1);
-      this.subject$.next(2);
-      this.subject$.next(3);
-    }
-
+  constructor() {}
 }
